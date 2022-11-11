8013903881d05990e4b504fa56cda14d432e3888
AMAN <antilaman3113@gmail.com>
2022-11-12 04:59:52 +0530

    filter option added

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-// fetch("https://restcountries.com/v3.1/all")
+// fetch("https://restcountries.com/v3.1/region/e")
 //   .then((resp) => resp.json())
 //   .then((data) => {
 //     console.log(data);
@@ -31,22 +31,21 @@ function renderCountries() {
 
 renderCountries();
 
-//  search country by name
-let inputEl = document.getElementById("search-country");
-inputEl.addEventListener("keyup", renderCountryByName);
+// search by region
 
-function renderCountryByName() {
+const regionInput = document.getElementById("region-list");
+
+regionInput.addEventListener("keyup", renderCountryByRegion);
+
+function renderCountryByRegion() {
   let html = "";
-  let countriesArray = [];
-  let nameValue = inputEl.value;
-  fetch(url)
-    .then((resp) => resp.json())
-    .then((data) => {
-      data.map((country) => {
-        if (country.name.common.toLowerCase().includes(nameValue)) {
-          countriesArray.push(country);
-        }
-        countriesArray.forEach((country) => {
+  let inputValue = regionInput.value.toLowerCase();
+  if (inputValue) {
+    let url = `https://restcountries.com/v3.1/region/${inputValue}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        data.forEach((country) => {
           html += `
             <div class="country-section">
                 <div class="image-section"><img src="${country.flags.png}" alt="country flag" /></div>
@@ -59,9 +58,40 @@ function renderCountryByName() {
               </div>
             `;
         });
-        console.log(html);
         mainDisplay.innerHTML = html;
       });
+  }
+}
+
+//  search country by name
+let inputEl = document.getElementById("search-country");
+inputEl.addEventListener("keyup", renderCountryByName);
+
+function renderCountryByName() {
+  let html = "";
+  let countriesArray = [];
+  let nameValue = inputEl.value;
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      data.forEach((country) => {
+        if (country.name.common.toLowerCase().includes(nameValue)) {
+          countriesArray.push(country);
+        }
+      });
+      countriesArray.forEach((country) => {
+        html += `
+          <div class="country-section">
+              <div class="image-section"><img src="${country.flags.png}" alt="country flag" /></div>
+              <div class="description-section">
+                <h2 class="country-name">${country.name.common}</h2>
+                <span class="population">Population: ${country.population}</span>
+                <span class="region">Region: ${country.region}</span>
+                <span class="capital">Capital: ${country.capital}</span>
+              </div>
+            </div>
+          `;
+      });
+      mainDisplay.innerHTML = html;
     });
-  console.log(countriesArray);
 }
