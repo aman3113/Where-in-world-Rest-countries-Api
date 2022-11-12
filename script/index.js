393f554428cb798fbe18c023edff8bd99d7ee838
AMAN <antilaman3113@gmail.com>
2022-11-12 23:53:12 +0530

    particular country section added

--- script/index.js
+++ script/index.js
@@ -14,15 +14,15 @@ function renderCountries() {
     .then((data) => {
       data.forEach((country) => {
         html += `
-        <div class="country-section">
-            <div class="image-section"><img src="${country.flags.png}" alt="country flag" /></div>
+        <div class="country-section" data-id="${country.name.common}">
+            <div class="image-section" data-id="${country.name.common}"><img src="${country.flags.png}" alt="country flag" /></div>
             <div class="description-section">
               <h2 class="country-name">${country.name.common}</h2>
               <span class="population">Population: ${country.population}</span>
               <span class="region">Region: ${country.region}</span>
               <span class="capital">Capital: ${country.capital}</span>
             </div>
-          </div>
+        </div>
         `;
       });
       mainDisplay.innerHTML = html;
@@ -30,6 +30,62 @@ function renderCountries() {
 }
 
 renderCountries();
+
+// display particular country details
+let detailSection = document.querySelector(".detailed-section");
+const mainCountrySection = document.querySelector(".main-country");
+const backBtn = document.getElementById("back-btn");
+
+document.addEventListener("click", function (e) {
+  let html = "";
+  let parentElement = e.target.parentElement;
+  let parentElementId = parentElement.dataset.id;
+  if (
+    parentElement.className === "country-section" ||
+    parentElement.className === "image-section"
+  ) {
+    detailSection.classList.remove("hidden");
+    let url = `https://restcountries.com/v2/name/${parentElementId}`;
+
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        let country = data[0];
+        console.log(country);
+        html = `
+        <div class="flag"><img src="${country.flags.png}" alt="country flag" /></div>
+        <div class="about">
+          <div class="name">${country.name}</div>
+          <div class="name-details">
+            <div class="name-details-left">
+              <p>Native Name: <span>${country.nativeName}</span></p>
+              <p>Population: <span>${country.population}</span></p>
+              <p>Region: <span>${country.region}</span></p>
+              <p>Sub Region: <span>${country.subregion}</span></p>
+              <p>Capital: <span>${country.capital}</span></p>
+            </div>
+            <div class="name-details-right">
+              <p>Top Level Domain: <span>${country.topLevelDomain}</span></p>
+              <p>Currencies: <span>${country.currencies[0].name}</span></p>
+              <p>Languages: <span>${country.languages[0].name}</span></p>
+            </div>
+          </div>
+          <div class="border-countries">
+            Border Countries:
+            <ul>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+        `;
+
+        mainCountrySection.innerHTML = html;
+        backBtn.addEventListener("click", function () {
+          detailSection.classList.add("hidden");
+        });
+      });
+  }
+});
 
 // search by region
 
